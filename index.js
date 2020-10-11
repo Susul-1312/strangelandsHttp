5e07f3e04a553aae77b4a514e5c6a0772d3464bc
Susul-1312 <sulpizh@gmail.com>
2020-10-11 18:22:57 +0200

    Repaired a mistake

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT | 80;
+const port = process.env.PORT || 80;
 
 var server = app.listen(port, () => console.log(`Listening on Port ${port}`));
 
